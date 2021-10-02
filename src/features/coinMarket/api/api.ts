b5bf448b3953bfde5a59fafbfae1316e076336e9
Alexandros Ntitoras <a.ntitoras@gmail.com>
2021-10-02 13:40:48 +0300

    remove try catch from api at coinmarket

--- src/features/coinMarket/api/api.ts
+++ src/features/coinMarket/api/api.ts
@@ -13,20 +13,16 @@ export type CoinApi = {
 
 const makeCoinMarketApi = ({ httpService }: MakeApi): CoinApi => {
   const getCoinsMarkets = async ({ page }: GetCoinsMarketsParams) => {
-    try {
-      const response = await httpService.GET(
-        `${APIPaths.COINS_MARKETS}`,
-        { vs_currency: DEFAULT_CURRENCY, per_page: NUMBER_OF_ITEMS_PER_PAGE, page },
-        {
-          'Content-Type': 'application/json'
-        }
-      );
-      const coins = response.data as CoinItem[];
+    const response = await httpService.GET(
+      `${APIPaths.COINS_MARKETS}`,
+      { vs_currency: DEFAULT_CURRENCY, per_page: NUMBER_OF_ITEMS_PER_PAGE, page },
+      {
+        'Content-Type': 'application/json'
+      }
+    );
+    const coins = response.data as CoinItem[];
 
-      return coins;
-    } catch (err) {
-      return err.response ? err.response.data.message : 'Unexpected Error';
-    }
+    return coins;
   };
 
   return { getCoinsMarkets };
