ea4b68fa3fdfa4f7a993ec9a9bea63b5a6ce59ca
Alexandros Ntitoras <a.ntitoras@gmail.com>
2021-10-03 23:03:50 +0300

    added deault color mode dark

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { ChakraProvider, theme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'src/routes';
+import { ColorModeSwitcher } from 'src/ColorModeSwitcher';
 
 const queryClient = new QueryClient();
 
-export const App = (): JSX.Element => (
-  <ChakraProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes></Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  </ChakraProvider>
-);
+export const App = (): JSX.Element => {
+  return (
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ColorModeSwitcher />
+        <BrowserRouter>
+          <Routes></Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
+};
