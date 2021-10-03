104a6661675eaa90689d957bc375f66eede3115b
Alexandros Ntitoras <a.ntitoras@gmail.com>
2021-10-03 14:55:05 +0300

    click coin and navigate to coins details

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'src/routes';
-import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory();
 const queryClient = new QueryClient();
 
 export const App = (): JSX.Element => (
   <ChakraProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
-      <Router history={history}>
+      <BrowserRouter>
         <Routes></Routes>
-      </Router>
+      </BrowserRouter>
     </QueryClientProvider>
   </ChakraProvider>
 );
